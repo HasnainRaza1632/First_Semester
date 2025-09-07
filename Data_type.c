cd7ac6eaf4d804e95ac294232c2a7840c4a09f2e
Hasnain Raza <chandiohasnain807@gmail.com>
2025-09-06 21:13:54 -0700

    basic_concepts
    learn about Data Types and Variables in C

---
+++ Data_type.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+        int a ; 
+        a = 10 ; 
+
+        // int a = 10 ; 
+
+        int b ; 
+        b = 5 ; 
+        
+        // int b = 5 ; 
+        float c = 1.5 ; 
+        float d ; 
+        d = 2.5 ; 
+        
+
+    return 0; // This is the end of the main function
+}
---
+++ Variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   int a ; //This is a variable declaration 
+   a = 10 ; //This is a variable initialization 
+   // How to print the value of a 
+   printf("The value of a is : %d" , a ) ; // This prints the value of a variable 
+   //%d is a format specifier for integers when i write %d it means that i want to print an integer 
+    return 0; 
+}
